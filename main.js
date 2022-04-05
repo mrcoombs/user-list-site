4845cfe53e77e876721d2b31666b95048a09a4a4
mrcoombs <matthewrcoombs@gmail.com>
2022-04-05 17:04:33 -0500

    Output to table
    Updated output to table format

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 const myForm = document.querySelector('#submission-box');
 const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email')
+const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+const tableHeader = document.querySelector('.table-header');
+const tableName = document.querySelector('.table-name');
+const tableEmail = document.querySelector('.table-email');
+const outputTable = document.querySelector('.output-table');
+let isTableHeader = false; 
 
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e){
     e.preventDefault();
     
+    //Confirm if values are input
     if(nameInput.value === '' || emailInput.value === ''){
-        //alert('Not all required fields are populated.')
         msg.innerHTML = 'Please enter all fields.';
         setTimeout(() => msg.remove(), 5000)
     }
     else{
-        //console.log("Success");
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        //Table header doesn't populate until first user entry
+        //Check if table header has been created
+        if(isTableHeader === false){
+            //Add table header
+            const th = document.createElement('th');
+            th.appendChild(document.createTextNode('Name'));
+            tableHeader.appendChild(th);
+            const th1 = document.createElement('th');
+            th1.appendChild(document.createTextNode('Email'));
+            tableHeader.appendChild(th1);
 
-        userList.appendChild(li);
+            //Add outline around table
+            outputTable.style.border ="solid";
+            outputTable.style.backgroundColor="lightgray";
+
+            //Set boolean to show table header is created
+            isTableHeader = true;
+        }
+
+        //Add record to table
+        const tr = document.createElement('tr');
+        const tdName = document.createElement('td');
+        const tdEmail = document.createElement('td');
+        tdName.appendChild(document.createTextNode(`${nameInput.value}`));
+        tdEmail.appendChild(document.createTextNode(`${emailInput.value}`));
+        tr.appendChild(tdName);
+        tr.appendChild(tdEmail)
+        outputTable.appendChild(tr);
 
         //Clear fields
         nameInput.value = '';
