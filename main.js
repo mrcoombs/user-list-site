3aaaa26f571d2674b789d10fa92f949101ffabbf
mrcoombs <matthewrcoombs@gmail.com>
2022-04-08 16:57:53 -0500

    Hide Empty Table
    Hide table if all user records deleted

--- main.js
+++ main.js
@@ -27,12 +27,15 @@ function onSubmit(e){
         if(isTableHeader === false){
             //Add table header
             const th = document.createElement('th');
+            th.className = 't-header';
             th.appendChild(document.createTextNode('Name'));
             tableHeader.appendChild(th);
             const th1 = document.createElement('th');
+            th1.className = 't-header';
             th1.appendChild(document.createTextNode('Email'));
             tableHeader.appendChild(th1);
             const th2 = document.createElement('th');
+            th2.className = 't-header';
             th2.appendChild(document.createTextNode('Delete'));
             tableHeader.appendChild(th2);
 
@@ -47,7 +50,6 @@ function onSubmit(e){
         //Add record to table
         const tr = document.createElement('tr');
         tr.className = "table-row" + increment;
-        console.log(tr.className);
         const tdName = document.createElement('td');
         const tdEmail = document.createElement('td');
         tdName.appendChild(document.createTextNode(`${nameInput.value}`));
@@ -80,7 +82,21 @@ function onSubmit(e){
 
 //Function to remove user from list
 function deleteUser(e){
-    //
+    //Initialize row and delete from parent node
     let deleteRow = document.querySelector('.'+e.className);
     deleteRow.parentNode.removeChild(deleteRow);
+
+    //Hide table if no users are left
+    if(outputTable.rows.length === 1){
+        let headerArray = tableHeader.getElementsByClassName("t-header");
+        let headerLength = headerArray.length;
+        for (let i = 0; i < headerLength; i++) {tableHeader.removeChild(headerArray[0]);}
+
+        //Set boolean to false so header will get added back in if more users are added
+        isTableHeader = false;
+
+        //Remove border and color
+        outputTable.style.border ="hidden";
+        outputTable.style.backgroundColor="gray";
+    }
 }
